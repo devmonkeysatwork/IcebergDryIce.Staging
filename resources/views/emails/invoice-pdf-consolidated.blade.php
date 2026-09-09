dc3e41328fe45f8c506a851fb2305381fdb5cdf9
touseef <touseefktk22@gmail.com>
2026-09-09 15:54:13 +0500

    invoice logo and styling change to match the old design

--- resources/views/emails/invoice-pdf-consolidated.blade.php
+++ resources/views/emails/invoice-pdf-consolidated.blade.php
@@ -27,23 +27,21 @@
 <!-- Header -->
 <table align="center" class="fullTable" bgcolor="#ffffff" style="border-radius: 10px 10px 0 0;">
     <tr>
-        <td style="padding: 20px;">
+        <td style="padding: 20px 20px 0px 20px;">
             <table width="100%">
                 <tr>
-                    <td style="width:55px;vertical-align: top;">
-                        <img src="{{asset('idi_logo.png')}}" width="54" height="54" alt="logo" />
+                    <td style="vertical-align: top;">
+                        <img src="{{asset('invoice-logo.jpg')}}" width="154" alt="logo" />
+                        <p style="font-family: Helvetica, Arial, sans-serif;font-size: 10px;color:#5E6470;">329 Churchill Avenue</p>
+                        <p style="font-family: Helvetica, Arial, sans-serif;font-size: 10px;color:#5E6470;">New Westminster BC V3L 4P5</p>
+                        <p style="font-family: Helvetica, Arial, sans-serif;font-size: 10px;color:#5E6470;">Orders 604-524-0609</p>
+                        <p style="font-family: Helvetica, Arial, sans-serif;font-size: 10px;color:#5E6470;">Office 604-524-0601</p>
                     </td>
-                    <td>
-                        <p style="font-weight: 700;font-size: 18px;color:#1A1C21;">Iceberg Dry Ice</p>
-                        <p style="font-size: 10px;color:#5E6470;">329 Churchill Avenue</p>
-                        <p style="font-size: 10px;color:#5E6470;">New Westminster BC V3L 4P5</p>
-                        <p style="font-size: 10px;color:#5E6470;">Orders 604-524-0609</p>
-                        <p style="font-size: 10px;color:#5E6470;">Office 604-524-0601</p>
-                    </td>
-                    <td style="vertical-align: top; text-align: right;">
-                        <p style="font-size: 36px; font-weight: 700; color:#B2B7C2; margin-top:10px;">Invoice</p>
-                        <p style="font-size: 12px; color:#5E6470;">Invoice Number: <strong>{{ $invoice->invoice_number }}</strong></p>
-                        <p style="font-size: 12px; color:#5E6470;">Customer Number: <strong>{{ $customer->id ?? 'N/A' }}</strong></p>
+                    <td style="vertical-align: top; text-align: right;padding-top: 30px;">
+                        <p style="font-family: Helvetica, Arial, sans-serif;font-size: 36px; font-weight: 300; color:#022a7f; margin-top:10px;">Invoice</p>
+                        <p style="font-family: Helvetica, Arial, sans-serif;font-size: 12px; font-weight: 900; color:#1a1a1a; margin-top:10px;">{{ \Carbon\Carbon::parse($invoice->invoice_date)->format('d-M-y') }}</p>
+                        <p style="font-family: Helvetica, Arial, sans-serif;font-size: 12px; color:#5E6470;">Invoice Number: <strong style="font-weight: 900;">{{ $invoice->invoice_number }}</strong></p>
+                        <p style="font-family: Helvetica, Arial, sans-serif;font-size: 12px; color:#5E6470;">Customer Number: <strong style="font-weight: 900;">{{ $customer->id ?? 'N/A' }}</strong></p>
                     </td>
                 </tr>
             </table>
@@ -54,25 +52,26 @@
 <!-- Billing Info -->
 <table align="center" class="fullTable" bgcolor="#ffffff">
     <tr>
-        <td style="padding: 20px;">
+        <td style="padding: 0px 20px 20px 20px">
             <table width="100%">
                 <tr>
                     <td style="vertical-align: top;">
-                        <p style="font-size: 10px;color:#5E6470;">Billed to</p>
-                        <p style="font-weight: 600;font-size: 10px;color:#1A1C21;">{{ $customer->name ?? 'N/A' }}</p>
-                        <p style="font-size: 10px;color:#5E6470;">{{ $customer->email ?? '' }}</p>
-                        <p style="font-size: 10px;color:#5E6470;">{{ $customer->phone ?? '' }}</p>
+{{--                        <p style="font-family: Helvetica, Arial, sans-serif;font-size: 10px;color:#5E6470;">Billed to</p>--}}
+                        <p style="font-weight: 900;font-family: Helvetica, Arial, sans-serif;font-size: 10px;color:#1A1C21;">{{ $customer->name ?? 'N/A' }}</p>
+                        <p style="font-family: Helvetica, Arial, sans-serif;font-size: 10px;color:#5E6470;">{{ $customer->address ?? '' }}.</p>
+                        <p style="font-family: Helvetica, Arial, sans-serif;font-size: 10px;color:#5E6470;">{{ $customer->city ?? '' }} {{ $customer->province ?? '' }}</p>
+                        <p style="font-family: Helvetica, Arial, sans-serif;font-size: 10px;color:#5E6470;">{{ $customer->postal_code ?? '' }}</p>
                     </td>
-                    <td style="vertical-align: top;">
-                        <p style="font-size: 10px;color:#5E6470;">Invoice Date</p>
-                        <p style="font-weight: 600;font-size: 10px;color:#1A1C21;">{{ \Carbon\Carbon::parse($invoice->invoice_date)->format('d.m.Y') }}</p>
-                    </td>
-                    <td style="vertical-align: top;text-align:right;">
-                        <p style="font-size: 10px;color:#5E6470;">Amount</p>
-                        <p style="color:#1A1C21;font-weight:700;font-size:20px;">
-                            ${{ number_format($totalAmount, 2) }}
-                        </p>
-                    </td>
+{{--                    <td style="vertical-align: top;">--}}
+{{--                        <p style="font-family: Helvetica, Arial, sans-serif;font-size: 10px;color:#5E6470;">Invoice Date</p>--}}
+{{--                        <p style="font-weight: 600;font-family: Helvetica, Arial, sans-serif;font-size: 10px;color:#1A1C21;">{{ \Carbon\Carbon::parse($invoice->invoice_date)->format('d.m.Y') }}</p>--}}
+{{--                    </td>--}}
+{{--                    <td style="vertical-align: top;text-align:right;">--}}
+{{--                        <p style="font-family: Helvetica, Arial, sans-serif;font-size: 10px;color:#5E6470;">Amount</p>--}}
+{{--                        <p style="color:#1A1C21;font-weight:700;font-family: Helvetica, Arial, sans-serif;font-size:20px;">--}}
+{{--                            ${{ number_format($totalAmount, 2) }}--}}
+{{--                        </p>--}}
+{{--                    </td>--}}
                 </tr>
             </table>
         </td>
@@ -85,28 +84,28 @@
         <td style="padding: 20px;">
             <table width="100%" border="1" cellspacing="0" cellpadding="5" style="border-color:#D7DAE0;">
                 <thead style="background-color:#f8f9fa;">
-                <tr>
-                    <th style="font-size: 9px; color:#5E6470; text-align:left;">Date</th>
-                    <th style="font-size: 9px; color:#5E6470; text-align:left;">PO#</th>
-                    <th style="font-size: 9px; color:#5E6470; text-align:center;">Quantity</th>
-                    <th style="font-size: 9px; color:#5E6470; text-align:left;">Item</th>
-                    <th style="font-size: 9px; color:#5E6470; text-align:center;">Price/Unit</th>
-                    <th style="font-size: 9px; color:#5E6470; text-align:right;">Product Cost</th>
-                    <th style="font-size: 9px; color:#5E6470; text-align:right;">GST</th>
-                    <th style="font-size: 9px; color:#5E6470; text-align:right;">PST</th>
+                <tr style="border-bottom: 3px solid #000;">
+                    <th style="font-family: Helvetica, Arial, sans-serif;font-size: 9px; color:#5E6470; text-align:left;">Date</th>
+                    <th style="font-family: Helvetica, Arial, sans-serif;font-size: 9px; color:#5E6470; text-align:left;">PO#</th>
+                    <th style="font-family: Helvetica, Arial, sans-serif;font-size: 9px; color:#5E6470; text-align:center;">Quantity</th>
+                    <th style="font-family: Helvetica, Arial, sans-serif;font-size: 9px; color:#5E6470; text-align:left;">Item</th>
+                    <th style="font-family: Helvetica, Arial, sans-serif;font-size: 9px; color:#5E6470; text-align:center;">Price/Unit</th>
+                    <th style="font-family: Helvetica, Arial, sans-serif;font-size: 9px; color:#5E6470; text-align:right;">Product Cost</th>
+                    <th style="font-family: Helvetica, Arial, sans-serif;font-size: 9px; color:#5E6470; text-align:right;">GST</th>
+                    <th style="font-family: Helvetica, Arial, sans-serif;font-size: 9px; color:#5E6470; text-align:right;">PST</th>
                 </tr>
                 </thead>
                 <tbody>
                 @foreach($lineItems as $item)
                     <tr>
-                        <td style="font-size: 10px; color:#1A1C21;">{{ $item->delivery_date ? \Carbon\Carbon::parse($item->delivery_date)->format('M d, y') : '' }}</td>
-                        <td style="font-size: 10px; color:#1A1C21;">{{ $item->order->po ?? '' }}</td>
-                        <td style="font-size: 10px; color:#1A1C21; text-align:center;">{{ $item->quantity }}</td>
-                        <td style="font-size: 10px; color:#1A1C21;">{{ $item->description }}</td>
-                        <td style="font-size: 10px; color:#1A1C21; text-align:center;">${{ number_format($item->unit_price, 2) }}</td>
-                        <td style="font-size: 10px; color:#1A1C21; text-align:right;">${{ number_format($item->total_price, 2) }}</td>
-                        <td style="font-size: 10px; color:#1A1C21; text-align:right;">${{ number_format($item->gst, 2) }}</td>
-                        <td style="font-size: 10px; color:#1A1C21; text-align:right;">${{ number_format($item->pst, 2) }}</td>
+                        <td style="font-family: Helvetica, Arial, sans-serif;font-size: 10px; color:#1A1C21;">{{ $item->delivery_date ? \Carbon\Carbon::parse($item->delivery_date)->format('M d, y') : '' }}</td>
+                        <td style="font-family: Helvetica, Arial, sans-serif;font-size: 10px; color:#1A1C21;">{{ $item->order->po ?? '' }}</td>
+                        <td style="font-family: Helvetica, Arial, sans-serif;font-size: 10px; color:#1A1C21; text-align:center;">{{ $item->quantity }}</td>
+                        <td style="font-family: Helvetica, Arial, sans-serif;font-size: 10px; color:#1A1C21;">{{ $item->description }}</td>
+                        <td style="font-family: Helvetica, Arial, sans-serif;font-size: 10px; color:#1A1C21; text-align:center;">${{ number_format($item->unit_price, 2) }}</td>
+                        <td style="font-family: Helvetica, Arial, sans-serif;font-size: 10px; color:#1A1C21; text-align:right;">${{ number_format($item->total_price, 2) }}</td>
+                        <td style="font-family: Helvetica, Arial, sans-serif;font-size: 10px; color:#1A1C21; text-align:right;">${{ number_format($item->gst, 2) }}</td>
+                        <td style="font-family: Helvetica, Arial, sans-serif;font-size: 10px; color:#1A1C21; text-align:right;">${{ number_format($item->pst, 2) }}</td>
                     </tr>
                 @endforeach
                 </tbody>
@@ -118,33 +117,38 @@
 <!-- Totals -->
 <table align="center" class="fullTable" bgcolor="#ffffff">
     <tr>
-        <td style="padding: 20px;">
-            <table width="100%">
+        <td style="padding-left: 20px;">
+            <p style="font-family: Helvetica, Arial, sans-serif;font-size:10px;font-weight:900;color:#022a7f;">GST #82379 8541</p>
+        </td>
+    </tr>
+    <tr>
+        <td style="padding: 20px;;">
+            <table width="150px">
                 <tr>
-                    <td style="font-size:10px;font-weight:700;color:#1A1C21;">Total Product Cost</td>
-                    <td style="font-size:10px;font-weight:700;color:#1A1C21;text-align:right;">${{ number_format($subTotal, 2) }}</td>
+                    <td style="font-family: Helvetica, Arial, sans-serif;font-size:10px;font-weight:700;color:#1A1C21;text-align: right">Total Product Cost</td>
+                    <td style="font-family: Helvetica, Arial, sans-serif;font-size:10px;font-weight:700;color:#1A1C21;text-align:right;">${{ number_format($subTotal, 2) }}</td>
                 </tr>
                 @foreach($flatCharges as $charge)
                     <tr>
-                        <td style="font-size:10px;font-weight:700;color:#1A1C21;">
+                        <td style="font-family: Helvetica, Arial, sans-serif;font-size:10px;font-weight:700;color:#1A1C21;text-align: right">
                             {{ $charge->label ?? \Illuminate\Support\Str::headline($charge->charge_key) }}
                         </td>
-                        <td style="font-size:10px;font-weight:700;color:#1A1C21;text-align:right;">
+                        <td style="font-family: Helvetica, Arial, sans-serif;font-size:10px;font-weight:700;color:#1A1C21;text-align:right;">
                             {{ $charge->amount < 0 ? '-' : '' }}${{ number_format(abs($charge->amount), 2) }}
                         </td>
                     </tr>
                 @endforeach
                 <tr>
-                    <td style="font-size:10px;font-weight:700;color:#1A1C21;">GST Tax</td>
-                    <td style="font-size:10px;font-weight:700;color:#1A1C21;text-align:right;">${{ number_format($gstTotal, 2) }}</td>
+                    <td style="font-family: Helvetica, Arial, sans-serif;font-size:10px;font-weight:700;color:#1A1C21;text-align: right">GST Tax</td>
+                    <td style="font-family: Helvetica, Arial, sans-serif;font-size:10px;font-weight:700;color:#1A1C21;text-align:right;">${{ number_format($gstTotal, 2) }}</td>
                 </tr>
                 <tr>
-                    <td style="font-size:10px;font-weight:700;color:#1A1C21;">PST Tax</td>
-                    <td style="font-size:10px;font-weight:700;color:#1A1C21;text-align:right;">${{ number_format($pstTotal, 2) }}</td>
+                    <td style="font-family: Helvetica, Arial, sans-serif;font-size:10px;font-weight:700;color:#1A1C21;text-align: right">PST Tax</td>
+                    <td style="font-family: Helvetica, Arial, sans-serif;font-size:10px;font-weight:700;color:#1A1C21;text-align:right;">${{ number_format($pstTotal, 2) }}</td>
                 </tr>
                 <tr>
-                    <td style="font-size:12px;font-weight:700;color:#1A1C21;border-top:1px solid #D7DAE0;padding-top:6px;">Invoice Total</td>
-                    <td style="font-size:12px;font-weight:700;color:#1A1C21;text-align:right;border-top:1px solid #D7DAE0;padding-top:6px;">${{ number_format($totalAmount, 2) }}</td>
+                    <td style="font-family: Helvetica, Arial, sans-serif;font-size:12px;font-weight:700;color:#1A1C21;border-top:1px solid #D7DAE0;padding-top:6px;text-align: right">Invoice Total</td>
+                    <td style="font-family: Helvetica, Arial, sans-serif;font-size:12px;font-weight:700;color:#1A1C21;text-align:right;border-top:1px solid #D7DAE0;padding-top:6px;">${{ number_format($totalAmount, 2) }}</td>
                 </tr>
             </table>
         </td>
@@ -156,9 +160,9 @@
 <table align="center" class="fullTable" bgcolor="#ffffff">
     <tr>
         <td style="padding: 20px;">
-            <p style="font-size:8px; font-weight:600; color:#5E6470;">Notes</p>
+            <p style="font-family: Helvetica, Arial, sans-serif;font-size:8px; font-weight:600; color:#5E6470;">Notes</p>
             <p style="border-top:1px solid #D7DAE0; margin:4px 0;"></p>
-            <p style="font-size:10px;color:#5E6470;white-space:pre-line;">{{ $invoice->notes }}</p>
+            <p style="font-family: Helvetica, Arial, sans-serif;font-size:10px;color:#5E6470;white-space:pre-line;">{{ $invoice->notes }}</p>
         </td>
     </tr>
 </table>
@@ -168,41 +172,41 @@
 <table align="center" class="fullTable" bgcolor="#ffffff">
     <tr>
         <td style="padding: 20px;">
-            <p style="font-size:8px; font-weight:600; color:#5E6470;">Terms</p>
-            <p style="border-top:1px solid #D7DAE0; margin:4px 0;"></p>
-            <p style="font-size:10px;color:#5E6470;">
-                Net 15 days, interest of 2% per month (24% per annum) charged on all overdue accounts.
+            <p style="font-family: Helvetica, Arial, sans-serif;font-size:11px; font-weight:900; color:rgba(2,42,127,0.87);">
+                TERMS: Net 15 days, interest of 2% per month (24% per annum) charged on all overdue accounts.
             </p>
-            <p style="font-size:10px;color:#5685e8;font-weight: 700;">
+{{--            <p style="border-top:1px solid #D7DAE0; margin:4px 0;"></p>--}}
+            <p style="font-family: Helvetica, Arial, sans-serif;font-size:12px;color:#3c79fa;font-weight: 500;">
                 All totes must be returned within two weeks of delivery unless arrangements have
                 been made with Iceberg. Late fees will apply.
             </p>
-            <p style="margin-top:20px;font-size:10px;color:#5E6470;">Thank you for your business!</p>
+            <p style="margin-top:20px;font-family: Helvetica, Arial, sans-serif;font-size:10px;color:#5E6470;">Thank you for your business!</p>
+            <a href="https://www.icebergdryice.com/" style="margin-top:20px;font-family: Helvetica, Arial, sans-serif;font-size:10px;color:#010101;font-weight:900;text-decoration: none;">www.icebergdryice.com</a>
         </td>
     </tr>
 </table>
 
 <!-- Business Info -->
-<table align="center" class="fullTable" bgcolor="#ffffff" style="border-radius:0 0 10px 10px;">
-    <tr>
-        <td style="padding: 20px;">
-            <p style="font-size:8px;font-weight:600;color:#5E6470;">Business Information</p>
-            <p style="border-top:1px solid #D7DAE0;margin:4px 0;"></p>
-            <table width="100%">
-                <tr>
-                    <td style="width:50%;">
-                        <p style="font-size:8px;font-weight:600;color:#1A1C21;">GST #</p>
-                        <p style="font-size:8px;color:#5E6470;">82379 8541</p>
-                    </td>
-                    <td>
-                        <p style="font-size:8px;font-weight:600;color:#1A1C21;">Contact</p>
-                        <p style="font-size:8px;color:#5E6470;text-decoration:underline;">admin@icebergdryice.com</p>
-                    </td>
-                </tr>
-            </table>
-        </td>
-    </tr>
-</table>
+{{--<table align="center" class="fullTable" bgcolor="#ffffff" style="border-radius:0 0 10px 10px;">--}}
+{{--    <tr>--}}
+{{--        <td style="padding: 20px;">--}}
+{{--            <p style="font-family: Helvetica, Arial, sans-serif;font-size:8px;font-weight:600;color:#5E6470;">Business Information</p>--}}
+{{--            <p style="border-top:1px solid #D7DAE0;margin:4px 0;"></p>--}}
+{{--            <table width="100%">--}}
+{{--                <tr>--}}
+{{--                    <td style="width:50%;">--}}
+{{--                        <p style="font-family: Helvetica, Arial, sans-serif;font-size:8px;font-weight:600;color:#1A1C21;">GST #</p>--}}
+{{--                        <p style="font-family: Helvetica, Arial, sans-serif;font-size:8px;color:#5E6470;">82379 8541</p>--}}
+{{--                    </td>--}}
+{{--                    <td>--}}
+{{--                        <p style="font-family: Helvetica, Arial, sans-serif;font-size:8px;font-weight:600;color:#1A1C21;">Contact</p>--}}
+{{--                        <p style="font-family: Helvetica, Arial, sans-serif;font-size:8px;color:#5E6470;text-decoration:underline;">admin@icebergdryice.com</p>--}}
+{{--                    </td>--}}
+{{--                </tr>--}}
+{{--            </table>--}}
+{{--        </td>--}}
+{{--    </tr>--}}
+{{--</table>--}}
 
 </body>
 </html>
